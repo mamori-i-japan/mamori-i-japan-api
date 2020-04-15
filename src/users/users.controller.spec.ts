d6c3063995cbf7cfe6fe12c41f879b5496e656a1
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2020-04-16 07:42:00 +0900

    :green_heart: Fix failed specs

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,13 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
 
 describe('Users Controller', () => {
   let controller: UsersController
+  const usersService = { findAll: () => ['test'] }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile()
+      providers: [UsersService],
+    })
+      .overrideProvider(UsersService)
+      .useValue(usersService)
+      .compile()
 
     controller = module.get<UsersController>(UsersController)
   })
