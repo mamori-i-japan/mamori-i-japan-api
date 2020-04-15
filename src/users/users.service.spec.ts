d9c25300471ed358fa63c550ea6aedbbdbe6a997
Yash Murty <yashmurty@gmail.com>
2020-04-15 17:41:57 +0900

    :construction: Fix user tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UsersService } from './users.service'
+import { UsersRepository } from './users.repository'
 
 describe('UsersService', () => {
   let service: UsersService
+  const usersRepository = { findAll: () => ['test'] }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile()
+      providers: [UsersService, UsersRepository],
+    })
+      .overrideProvider(UsersRepository)
+      .useValue(usersRepository)
+      .compile()
 
     service = module.get<UsersService>(UsersService)
   })
