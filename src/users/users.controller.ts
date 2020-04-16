1d061256e324931a41e7cc21d42dd3e8856111ab
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2020-04-16 16:03:22 +0900

    :bug: Use FirebaseNormalUserValidateGuard for GET /users/me/temp_ids

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,12 @@ import { Controller, Get, Request, UseGuards } from '@nestjs/common'
 import {
   ApiOperation,
   ApiBearerAuth,
-  ApiHeader,
   ApiOkResponse,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger'
 import { UsersService } from './users.service'
 import { TempID } from './interfaces/temp-id.interface'
-import { FirebaseNormalUserLoginGuard } from '../auth/guards/firebase-normal-user-login.guard'
-import { X_MOBILE_SECRET_RANDOM_TOKEN_HEADER } from '../auth/constants'
+import { FirebaseNormalUserValidateGuard } from '../auth/guards/firebase-normal-user-validate.guard'
 
 @Controller('users')
 export class UsersController {
@@ -17,10 +15,9 @@ export class UsersController {
 
   @ApiOperation({ summary: "Find users' own TempIDs" })
   @ApiBearerAuth()
-  @ApiHeader({ name: X_MOBILE_SECRET_RANDOM_TOKEN_HEADER, required: true })
   @ApiOkResponse()
   @ApiUnauthorizedResponse()
-  @UseGuards(FirebaseNormalUserLoginGuard)
+  @UseGuards(FirebaseNormalUserValidateGuard)
   @Get('/me/temp_ids')
   async getMeTempIDs(@Request() req): Promise<TempID[]> {
     const userId = req.user.uid
