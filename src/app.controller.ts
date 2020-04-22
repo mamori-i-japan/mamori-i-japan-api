153d8399488a0205e3276ee3c39c3183e84675fb
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2020-04-22 13:55:44 +0900

    :shower: Remove unused endpoint GET /profile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
-import { FirebaseNormalUserValidateGuard } from './auth/guards/firebase-normal-user-validate.guard'
 
 @Controller()
 export class AppController {
@@ -8,14 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log('WIP - getHello function')
-
     return this.appService.getHello()
-  }
-
-  @UseGuards(FirebaseNormalUserValidateGuard)
-  @Get('profile')
-  getProfileFirebase(@Request() req) {
-    return req.user
   }
 }
