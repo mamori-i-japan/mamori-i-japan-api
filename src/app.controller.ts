058f376d6223278e723c8de5286779af2deec5ea
Yash Murty <yashmurty@gmail.com>
2020-04-24 16:55:00 +0900

    :construction: Add logger to app service as example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,12 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
-import { AppLogger } from './shared/logger/logger.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private appLogger: AppLogger) {
-    this.appLogger.setContext(AppController.name)
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    this.appLogger.debug('Logged from getHello controller function')
-
     return this.appService.getHello()
   }
 }
