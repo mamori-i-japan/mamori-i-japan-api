50da197cd34781a12abd977725b578287cabda45
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2020-04-24 13:51:58 +0900

    :shell: npx nest generate interceptor shared/created-response

---
+++ src/shared/created-response.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { CreatedResponseInterceptor } from './created-response.interceptor';
+
+describe('CreatedResponseInterceptor', () => {
+  it('should be defined', () => {
+    expect(new CreatedResponseInterceptor()).toBeDefined();
+  });
+});
