b55869ec09ee0421235087aee073e1490bf13d34
Yash Murty <yashmurty@gmail.com>
2020-04-28 00:35:00 +0900

    :construction: Use logging interceptor

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config'
 import { configModuleOptions } from '../shared/config/module-options'
 import { FirebaseModule } from './firebase/firebase.module'
 import { LoggerModule } from './logger/logger.module'
+import { LoggingInterceptor } from './interceptors/logging.interceptor'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
   imports: [ConfigModule.forRoot(configModuleOptions), FirebaseModule, LoggerModule],
-  providers: [],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
   exports: [ConfigModule, FirebaseModule, LoggerModule],
 })
 export class SharedModule {}
