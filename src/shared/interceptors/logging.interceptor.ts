411085e330b6d8f1059ebdeef108ea89a1c92b31
Yash Murty <yashmurty@gmail.com>
2020-04-28 00:47:41 +0900

    :construction: Add basic logging for requests

--- src/shared/interceptors/logging.interceptor.ts
+++ src/shared/interceptors/logging.interceptor.ts
@@ -1,20 +1,31 @@
-import {
-  CallHandler,
-  ExecutionContext,
-  Injectable,
-  NestInterceptor,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { tap } from 'rxjs/operators'
+import { AppLogger } from '../logger/logger.service'
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+  constructor(private appLogger: AppLogger) {
+    this.appLogger.setContext(LoggingInterceptor.name)
+  }
 
-    const now = Date.now();
-    return next
-      .handle()
-      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest()
+    const method = request.method
+    const url = request.originalUrl
+    this.appLogger.debug(`method: ${method} | url: ${url}`)
+
+    const now = Date.now()
+    return next.handle().pipe(
+      tap(() => {
+        const response = context.switchToHttp().getResponse()
+        const statusCode = response.statusCode
+
+        const responseTime = Date.now() - now
+        this.appLogger.debug(
+          `method: ${method} | url: ${url} | statusCode: ${statusCode} | responseTime: ${responseTime}ms`
+        )
+      })
+    )
   }
 }
