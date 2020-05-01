9929cc9e50ed3270d9efbf57d26b7960895182c2
Yash Murty <yashmurty@gmail.com>
2020-05-01 21:32:40 +0900

    :construction: Fix tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,17 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UsersService } from './users.service'
 import { UsersRepository } from './users.repository'
+import { FirebaseService } from '../shared/firebase/firebase.service'
 
 describe('UsersService', () => {
   let service: UsersService
   const usersRepository = { findAll: () => ['test'] }
+  const firebaseService = {}
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, UsersRepository],
+      providers: [UsersService, UsersRepository, FirebaseService],
     })
       .overrideProvider(UsersRepository)
       .useValue(usersRepository)
+      .overrideProvider(FirebaseService)
+      .useValue(firebaseService)
       .compile()
 
     service = module.get<UsersService>(UsersService)
