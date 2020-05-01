8a5324b99dca5175355fc48b49a4c9e5fc5f93e9
Yash Murty <yashmurty@gmail.com>
2020-05-01 10:58:01 +0900

    :construction: Update org repo

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -19,3 +19,20 @@ export class CreateOrganizationRequestDto {
   addedByAdminUserId: string
   addedByAdminEmail: string
 }
+
+export class UpdateOrganizationRequestDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  name: string
+
+  @ApiPropertyOptional({ example: 'This is optional message. Can be later added via PATCH.' })
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  message: string
+
+  // Keys without any decorators are non-Whitelisted. Validator will throw error if it's passed in payload.
+  id: string
+}
