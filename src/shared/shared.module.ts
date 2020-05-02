31e39864f53875c65e88c57772d09d4f1f78eb99
Yash Murty <yashmurty@gmail.com>
2020-05-03 01:55:03 +0900

    :construction: Add all exceptions filter

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -4,11 +4,18 @@ import { configModuleOptions } from '../shared/config/module-options'
 import { FirebaseModule } from './firebase/firebase.module'
 import { LoggerModule } from './logger/logger.module'
 import { LoggingInterceptor } from './interceptors/logging.interceptor'
-import { APP_INTERCEPTOR } from '@nestjs/core'
+import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core'
+import { AllExceptionsFilter } from './filters/all-exceptions.filter'
 
 @Module({
   imports: [ConfigModule.forRoot(configModuleOptions), FirebaseModule, LoggerModule],
-  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
   exports: [ConfigModule, FirebaseModule, LoggerModule],
 })
 export class SharedModule {}
