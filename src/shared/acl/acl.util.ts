1630b12eb3ff765578e2843d2e2010e041e34e5b
Yash Murty <yashmurty@gmail.com>
2020-05-03 16:22:22 +0900

    :construction: ACL - Util functions

--- src/shared/acl/acl.util.ts
+++ src/shared/acl/acl.util.ts
@@ -5,6 +5,10 @@ import {
   organizationAdminKey,
 } from './acl.constants'
 
+/**
+ * getXXXAdminACLEntry function returns the keys that need to be added to
+ * the accessControlList of a resource.
+ */
 export function getSuperAdminACLEntry() {
   return superAdminKey
 }
@@ -22,17 +26,27 @@ export function getOrganizationAdminACLEntry(organizationId: string) {
 }
 
 /**
- * This function is used when inviting new admin users with admin roles.
- * It will be used to prevent organization admin user to create a super admin, etc.
+ * canUserEditResource function is used to check if a user can view/edit a resource
  */
-export function canUserEditSuperAdmin(userAccessKey: string): boolean {
+export function canUserEditResource(userAccessKey: string, accessControlList: string[]): boolean {
+  // WIP
+  return false
+}
+
+/**
+ * canUserCreateXXX function is used when inviting new admin users with admin roles.
+ * - It will prevent organization admin user to create a super admin, etc.
+ */
+export function canUserCreateSuperAdmin(userAccessKey: string): boolean {
+  // Checks if the user performing an action has superAdmin access.
   if (userAccessKey === superAdminKey) {
     return true
   }
   return false
 }
 
-export function canUserEditNationalAdmin(userAccessKey: string): boolean {
+export function canUserCreateNationalAdmin(userAccessKey: string): boolean {
+  // Checks if the user performing an action has nationalAdmin access or above.
   if (userAccessKey === superAdminKey) {
     return true
   }
@@ -42,7 +56,8 @@ export function canUserEditNationalAdmin(userAccessKey: string): boolean {
   return false
 }
 
-export function canUserEditPrefectureAdmin(userAccessKey: string, prefectureId: string): boolean {
+export function canUserCreatePrefectureAdmin(userAccessKey: string, prefectureId: string): boolean {
+  // Checks if the user performing an action has prefectureAdmin access or above.
   if (userAccessKey === superAdminKey) {
     return true
   }
@@ -55,10 +70,11 @@ export function canUserEditPrefectureAdmin(userAccessKey: string, prefectureId: 
   return false
 }
 
-export function canUserEditOrganizationAdmin(
+export function canUserCreateOrganizationAdmin(
   userAccessKey: string,
   organizationId: string
 ): boolean {
+  // Checks if the user performing an action has organizationAdmin access or above.
   if (userAccessKey === superAdminKey) {
     return true
   }
