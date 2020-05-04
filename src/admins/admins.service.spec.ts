7999c0820b81ce53c36739a14adf742bf77061dc
Yash Murty <yashmurty@gmail.com>
2020-05-04 17:10:22 +0900

    :construction: Fix tests

--- src/admins/admins.service.spec.ts
+++ src/admins/admins.service.spec.ts
@@ -1,17 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AdminsService } from './admins.service'
 import { AdminsRepository } from './admins.repository'
+import { OrganizationsService } from '../organizations/organizations.service'
 
 describe('AdminsService', () => {
   let service: AdminsService
   const adminsRepository = { findAll: () => ['test'] }
+  const organizationsService = {}
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminsService, AdminsRepository],
+      providers: [AdminsService, AdminsRepository, OrganizationsService],
     })
       .overrideProvider(AdminsRepository)
       .useValue(adminsRepository)
+      .overrideProvider(OrganizationsService)
+      .useValue(organizationsService)
       .compile()
 
     service = module.get<AdminsService>(AdminsService)
