a5cbd3a8405959b899bd6e507841a2546800195c
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2020-05-06 16:19:44 +0900

    :bug: Fix request body randomID to Array

--- src/users/dto/delete-user-organization.dto.ts
+++ src/users/dto/delete-user-organization.dto.ts
@@ -1,12 +1,21 @@
-import { ApiPropertyOptional } from '@nestjs/swagger'
-import { IsString, IsOptional, IsNotEmpty } from 'class-validator'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { IsString, IsOptional, IsNotEmpty, IsArray, ValidateNested } from 'class-validator'
+import { Type } from 'class-transformer'
 
-export class DeleteUserOrganizationDto {
+export class RandomIDDto {
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   randomID: string
+}
+
+export class DeleteUserOrganizationDto {
+  @ApiProperty({ type: RandomIDDto, isArray: true })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => RandomIDDto)
+  randomIDs: RandomIDDto[]
 
   // Keys without any decorators are non-Whitelisted. Validator will throw error if it's passed in payload.
   userId: string
