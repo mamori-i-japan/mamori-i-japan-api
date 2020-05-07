8cdb750ef0d5bf67781ca627db9a3f1592aa24a9
Yash Murty <yashmurty@gmail.com>
2020-05-07 23:39:29 +0900

    :construction: Fix tests

--- src/admins/admins.service.spec.ts
+++ src/admins/admins.service.spec.ts
@@ -3,16 +3,24 @@ import { AdminsService } from './admins.service'
 import { AdminsRepository } from './admins.repository'
 import { OrganizationsService } from '../organizations/organizations.service'
 import { PrefecturesService } from '../prefectures/prefectures.service'
+import { FirebaseService } from '../shared/firebase/firebase.service'
 
 describe('AdminsService', () => {
   let service: AdminsService
   const adminsRepository = { findAll: () => ['test'] }
   const organizationsService = {}
   const prefecturesService = {}
+  const firebaseService = {}
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminsService, AdminsRepository, OrganizationsService, PrefecturesService],
+      providers: [
+        AdminsService,
+        AdminsRepository,
+        OrganizationsService,
+        PrefecturesService,
+        FirebaseService,
+      ],
     })
       .overrideProvider(AdminsRepository)
       .useValue(adminsRepository)
@@ -20,6 +28,8 @@ describe('AdminsService', () => {
       .useValue(organizationsService)
       .overrideProvider(PrefecturesService)
       .useValue(prefecturesService)
+      .overrideProvider(FirebaseService)
+      .useValue(firebaseService)
       .compile()
 
     service = module.get<AdminsService>(AdminsService)
