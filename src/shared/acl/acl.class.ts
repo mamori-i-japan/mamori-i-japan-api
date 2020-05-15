2f83c6846e247cd8bbb5d1df2f252a5f115bd1b9
Yash Murty <yashmurty@gmail.com>
2020-05-15 18:05:07 +0900

    :construction: Update swagger response for ACL

--- src/shared/acl/acl.class.ts
+++ src/shared/acl/acl.class.ts
@@ -1,3 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class ResourceWithACL {
+  @ApiProperty({
+    example: ['SUPER_ADMIN_KEY', 'NATIONAL_ADMIN_KEY', 'PREFECTURE_ADMIN_KEY|1'],
+  })
   accessControlList: string[]
 }
